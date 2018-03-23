f6fe713e55aa14c301cc4826de9aad94ed7a6c33
Anish Karandikar <anishkny@gmail.com>
2018-03-23 18:31:07 -0400

    Simplify tests

--- test/cli-test.js
+++ test/cli-test.js
@@ -2,23 +2,15 @@ const assert = require('assert');
 const shell = require('shelljs');
 const tempdir = require('tempdir');
 
-const origdir = process.cwd();
-
 describe('CLI tests', async () => {
 
   before(async () => {
     shell.config.verbose = true;
-    console.log(`Original dir: [${origdir}]`);
-    const workdir = await tempdir();
-    shell.cd(workdir);
-    console.log(`Changed to: [${process.cwd()}]`);
   });
 
-  it('should install npm package', async () => {
-    shell.exec(`npm install --silent yarn`);
-    shell.exec(`npx yarn add 'file:${origdir}'`);
-    const runoutput = shell.exec(`npx yarn run webgif`);
-    assert(runoutput.code === 0, runoutput)
+  it('should run webgif', async () => {
+    const runoutput = shell.exec(`node index.js`);
+    assert(runoutput.code === 0, runoutput);
   });
 
 });
