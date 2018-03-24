910d833f985b71bf72b87aa4279c0c21abd4a660
Anish Karandikar <anishkny@gmail.com>
2018-03-23 20:34:31 -0400

    Refactor async logic

--- index.js
+++ index.js
@@ -35,35 +35,28 @@ const argv = require('yargs')
 
   console.log(`Navigating to URL: ${argv.url}`);
   process.stdout.write('Taking screenshots: .');
+  const screenshotPromises = [];
   screenshotTaker = setInterval(async () => {
     if (page) {
       filename = `${workdir}/T${new Date().getTime()}.png`;
       process.stdout.write('.');
-      await page.screenshot({ path: filename, });
+      screenshotPromises.push(page.screenshot({ path: filename, }));
     }
   }, 1000);
 
   await page.goto(argv.url);
-  await delay(argv.duration * 1000);
-  clearInterval(screenshotTaker);
-  await delay(2000);
-  await page.close();
-  await browser.close();
+  setTimeout(async () => {
+    clearInterval(screenshotTaker);
+    await Promise.all(screenshotPromises);
+    await page.close();
+    await browser.close();
+    console.log(`\nEncoding GIF: ${argv.output}`);
+    const encoder = new GIFEncoder(1024, 768);
+    await pngFileStream(`${workdir}/T*png`)
+      .pipe(encoder.createWriteStream({ repeat: 0, delay: 200, quality: 20 }))
+      .pipe(fs.createWriteStream(`${argv.output}`));
+  }, argv.duration * 1000)
 
-  console.log(`\nEncoding GIF: ${argv.output}`);
-  const encoder = new GIFEncoder(1024, 768);
-  await pngFileStream(`${workdir}/T*png`)
-    .pipe(encoder.createWriteStream({ repeat: 0, delay: 200, quality: 20 }))
-    .pipe(fs.createWriteStream(`${argv.output}`));
 })();
 
-
-function delay(time) {
-  return new Promise(function(fulfill) {
-    setTimeout(fulfill, time);
-  });
-}
-
-process.on('unhandledRejection', function(reason, p) {
-  // console.log("Possibly Unhandled Rejection at: Promise ", p, " reason: ", reason);
-});
+process.on('unhandledRejection', function(reason, p) {});
